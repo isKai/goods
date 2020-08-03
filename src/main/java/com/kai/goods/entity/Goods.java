0b798852e9510b5eb68d470ded63fbbdaa96f267
isKai <kaichen@jmu.edu.cn>
2020-08-03 16:50:09 +0800

    修改Jenkinsfile

--- src/main/java/com/kai/goods/entity/Goods.java
+++ src/main/java/com/kai/goods/entity/Goods.java
@@ -1,4 +1,54 @@
 package com.kai.goods.entity;
 
 public class Goods {
+
+    int id;
+
+    String title;
+
+    String price;
+
+    String image;
+
+    String brand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
--- src/main/java/com/kai/goods/mapper/GoodsMapper.java
+++ src/main/java/com/kai/goods/mapper/GoodsMapper.java
@@ -1,4 +1,19 @@
 package com.kai.goods.mapper;
 
+import com.kai.goods.entity.Goods;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public class GoodsMapper {
+     String tableName = "goods";
+     String insertField = "title, price, image, brand";
+     String selectField = "id" + insertField;
+
+    @Select({"select", selectField, "from", tableName})
+    List<Goods> selectAll();
+
+
 }
